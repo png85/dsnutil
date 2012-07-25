ebfdad5f41de165d71dd9484598d71233ef706ea
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-07-25 20:03:24 +0200

    (Singleton.hh) move singleton template to namespace DSN

--- Singleton.hh
+++ Singleton.hh
@@ -5,34 +5,37 @@
 #ifndef SINGLETON_HH
 #define SINGLETON_HH 1
 
-/// \brief Template for singleton classes
-///
-/// This template can be used to implement the "singleton" design pattern
-/// on any class.
-template <class Derived> class Singleton {
-public:
-  /// \brief Access singleton instance
+namespace DSN {
+  /// \brief Template for singleton classes
   ///
-  /// Returns a reference to the instance of this singleton.
-  static Derived& getInstance() {
-    static Derived instance;
-    return instance;
-  }
+  /// This template can be used to implement the "singleton" design pattern
+  /// on any class.
+  template <class Derived> class Singleton {
+  public:
+    /// \brief Access singleton instance
+    ///
+    /// Returns a reference to the instance of this singleton.
+    static Derived& getInstance() {
+      static Derived instance;
+      return instance;
+    }
 
-protected:
-  /// \brief Default constructor
-  ///
-  /// \note This ctor is protected so that derived classes can implement
-  /// their own logics for object initialization while still maintaining
-  /// the impossibility of direct ctor calls!
-  Singleton() {}
+  protected:
+    /// \brief Default constructor
+    ///
+    /// \note This ctor is protected so that derived classes can implement
+    /// their own logics for object initialization while still maintaining
+    /// the impossibility of direct ctor calls!
+    Singleton() {}
 
-private:
-  /// \brief Copy constructor
-  ///
-  /// \note This ctor is private to prevent multiple instances of the same
-  /// singleton from being created through object assignments!
-  Singleton(const Singleton&) {}
+  private:
+    /// \brief Copy constructor
+    ///
+    /// \note This ctor is private to prevent multiple instances of the same
+    /// singleton from being created through object assignments!
+    Singleton(const Singleton&) {}
+  };
+
 };
 
 #endif // !SINGLETON_HH
