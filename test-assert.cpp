b233d2a51e9ee8c87c923a6d5381aa2da225f793
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-10-24 12:01:14 +0200

    add assert(3) that throws exceptions
    this commit adds a new header 'assert.hpp' which redefines the assert(3)
    macro known from C. the new macro throws an std::logic_error exception with
    a message that gives more precise debugging info and can be handled at
    runtime instead of completely aborting program execution.

---
+++ test-assert.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <cstdlib>
+
+#include "assert.hpp"
+
+int main(int argc, char** argv) {
+  std::cout << "assert.hpp throwing assert() test" << std::endl;
+  try {
+    assert(1 == 0);
+  }
+
+  catch (const std::exception& ex) {
+    std::cout << "Got exception: " << ex.what() << std::endl;
+    return EXIT_SUCCESS;
+  }
+
+  return EXIT_FAILURE;
+}
