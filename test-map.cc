ec9a0c59a04bafa5e3b95f7c8c188fb292152afc
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-10-02 16:11:07 +0200

    improve test-map.cc
    this commit improves the test program for map.hh by making use of
    pretty_print.hh and assert.hh

--- test-map.cc
+++ test-map.cc
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 
+#include "assert.hh"
 #include "map.hh"
+#include "pretty_print.hh"
 
 int main(void) {
     std::map<std::string, float> stocks = {
@@ -13,62 +15,49 @@ int main(void) {
         std::pair<std::string, float>("ACME", 1.5)
     };
 
-    std::function<void (std::pair<std::string, float>)> map_dump = [&](std::pair<std::string, float> p) {
-        std::cout << "stocks['" << p.first << "'] = " << p.second << std::endl;
-    };
-
     std::function<void (std::string)> vector_dump = [&](std::string k) {
         std::cout << "stocks['" << k << "'] = " << stocks[k] << std::endl;
     };
 
-    std::cout << "Unsorted map at start:" << std::endl;
-    std::for_each(stocks.begin(), stocks.end(), map_dump);
-    std::cout << std::endl;
+    std::cout << "Unsorted map at start: " << stocks << std::endl;
 
 
     // extract map keys into existing vector
-    std::cout << "Extracting map keys(1):" << std::endl;
     std::vector<std::string> k1;
     DSN::map_keys(stocks, k1);
-    std::for_each(k1.begin(), k1.end(), [&](std::string s) { std::cout << s << std::endl; });
+    std::cout << "Unsorted map keys in existing vector: " << k1 << "; map data is:" << std::endl;
+    std::for_each(k1.begin(), k1.end(), vector_dump);
 
 
     // extract map keys into new vector
-    std::cout << "Extracting map keys(2):" << std::endl;
-    std::vector<std::string> k2 = DSN::map_keys(stocks);
-    std::for_each(k2.begin(), k2.end(), [&](std::string s) { std::cout << s << std::endl; });
-    std::cout << std::endl;
+    std::cout << "Unsorted map keys in new vector: " << DSN::map_keys(stocks) << std::endl;
+    assert(k1 == DSN::map_keys(stocks));
 
 
     // sort map keys into existing vector
-    std::cout << "Sorting keys in ascending order(1):" << std::endl;
-    std::vector<std::string> k3;
-    DSN::map_sort(stocks, k3);
-    std::for_each(k3.begin(), k3.end(), vector_dump);
+    std::vector<std::string> k2;
+    DSN::map_sort(stocks, k2);
+    std::cout << "Sorted map keys in existing vector: " << k2 << "; map data is:" << std::endl;
+    std::for_each(k2.begin(), k2.end(), vector_dump);
 
 
     // sort map keys into new vector
-    std::cout << "Sorting keys in ascending order(2):" << std::endl;
-    std::vector<std::string> k4 = DSN::map_sort(stocks);
-    std::for_each(k4.begin(), k4.end(), vector_dump);
-
+    std::cout << "Sorted map keys in new vector: " << DSN::map_sort(stocks) << std::endl;
+    assert(k2 == DSN::map_sort(stocks));
 
     std::function<bool (std::string, std::string)> desc_sort = [&](std::string a, std::string b) {
         return (stocks[a] > stocks[b]);
     };
 
     // sort map keys into existing vector w/ custom comparator
-    std::cout << "Sorting keys in descending order(1):" << std::endl;
-    std::vector<std::string> k5;
-    DSN::map_sort(stocks, k5, desc_sort);
-    std::for_each(k5.begin(), k5.end(), vector_dump);
-
+    std::vector<std::string> k3;
+    DSN::map_sort(stocks, k3, desc_sort);
+    std::cout << "Sorted map keys (descending) in existing vector: " << k3 << "; map data is:" << std::endl;
+    std::for_each(k3.begin(), k3.end(), vector_dump);
 
     // sort map keys into new vector w/ custom comparator
-    std::cout << "Sorting keys in descending order(2):" << std::endl;
-    std::vector<std::string> k6 = DSN::map_sort(stocks, desc_sort);
-    std::for_each(k6.begin(), k6.end(), vector_dump);
-
+    std::cout << "Sorted map keys (descending) in new vector: " << DSN::map_sort(stocks, desc_sort) << std::endl;
+    assert(k3 == DSN::map_sort(stocks, desc_sort));
 
     return 0;
 }
